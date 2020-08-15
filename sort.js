ec6d9c391a9cff3872ebe338f5d059fe91cfd6cf
GAURAV <2018.gaurav.patil@ves.ac.in>
2020-08-15 12:54:50 +0530

    day3

--- sort.js
+++ sort.js
@@ -8,4 +8,43 @@ document.querySelectorAll('.choice').forEach(item => {
         item.classList.add("selected");
        
     })
-  })
+})
+
+var slider = document.getElementById("myRange");
+var box = document.querySelector('.box');
+var boxp = document.querySelector('.box p');
+
+
+slider.oninput = function(){
+    var x = this.value;
+    
+    box.style.left = this.value + "%";
+    boxp.textContent = this.value;
+    slider.style.background = "-webkit-linear-gradient(left,rgb(var(--blue-crayola)) "+x+"%, rgba(var(--rich-black-fogra-29),0.2) "+x+"%)";
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temp, randomIndex;
+  
+    
+    while (currentIndex !== 0) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      temp = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temp;
+    }
+  
+    return array;
+  }
+document.querySelector(".Startbtn").addEventListener("click",function(){
+    var arr = [];
+    var N = slider.value;
+    for (var i = 1; i <= N; i++) {
+        arr.push(i);
+    }
+    arr= shuffle(arr);
+    
+});
