98e0317ff694b332c47957ba4ab8c81f53c3f197
GAURAV <2018.gaurav.patil@ves.ac.in>
2020-08-16 22:46:39 +0530

    bubblesort done

--- sort.js
+++ sort.js
@@ -10,9 +10,20 @@ document.querySelectorAll('.choice').forEach(item => {
     })
 })
 
+
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+
 var slider = document.getElementById("myRange");
 var box = document.querySelector('.box');
 var boxp = document.querySelector('.box p');
+
+
+
+
+
 
 
 slider.oninput = function(){
@@ -24,10 +35,10 @@ slider.oninput = function(){
 }
 function cratebargraph(array,N){
     var pdiv = document.querySelector(".sortgraph")
-    for(var i=1;i<N;i++){
-        var val = 100-((array[i])*100/N);
+    for(var i=1;i<=N;i++){
+        var val = 100-((array[i-1])*100/N);
         var wid = 90/N;
-        console.log(i + " " + array[i])
+        
         var div = document.createElement('div');
         div.setAttribute('id', "val"+i);
         div.classList.add("bar");
@@ -55,20 +66,77 @@ function shuffle(array) {
     }
   
     return array;
-  }
-  var arr = [];
+}
+
+
+
+
+var arr = [];
 for (var i = 1; i <= 50; i++) {
     arr.push(i);
 }
 arr= shuffle(arr); 
 console.log(arr);
 cratebargraph(arr,50);
+
+
+ async function bubbleSort(array){
+    let len = array.length;
+    console.log(len);
+    
+    var myNode = document.querySelector(".sortgraph").childNodes;
+
+    for (let i = 1; i <= len; i++) {
+        
+        for (let j = 1; j <=len-i; j++) {
+            
+            
+            
+            var val2 = 100-((array[j])*100/len);
+            var val1 = 100-((array[j-1])*100/len);
+            
+            var f= 0;
+            var ind = j+1;
+
+
+            document.getElementById("val"+j).style.background="-webkit-linear-gradient(top,rgba(0,0,0,0) "+val1+"%, rgb(var(--minion-yellow)) "+val1+"%)";
+            document.getElementById("val"+ind).style.background="-webkit-linear-gradient(top,rgba(0,0,0,0) "+val2+"%, rgb(var(--minion-yellow)) "+val2+"%)";
+            if (array[j-1] > array[j]) {
+                let tmp = array[j-1];
+                array[j-1] = array[j];
+                array[j] = tmp;
+                f=1;
+            }
+            await sleep(100);
+            if(f===1){
+                console.log("swapping "+ arr[j-1]+arr[j]);
+                document.getElementById("val"+ind).style.background="-webkit-linear-gradient(top,rgba(0,0,0,0) "+val1+"%, rgb(var(--blue-crayola)) "+val1+"%)";
+                document.getElementById("val"+j).style.background="-webkit-linear-gradient(top,rgba(0,0,0,0) "+val2+"%, rgb(var(--blue-crayola)) "+val2+"%)";
+            }else{
+                console.log(" not swapping "+ arr[j-1]+arr[j]);
+                document.getElementById("val"+ind).style.background="-webkit-linear-gradient(top,rgba(0,0,0,0) "+val2+"%, rgb(var(--blue-crayola)) "+val2+"%)";
+                document.getElementById("val"+j).style.background="-webkit-linear-gradient(top,rgba(0,0,0,0) "+val1+"%, rgb(var(--blue-crayola)) "+val1+"%)";
+            }
+            
+            
+
+
+            
+        }
+        
+    }
+    console.log("done");
+
+}
+
+
+
 document.querySelector(".Startbtn").addEventListener("click",function(){
 
     
      var N = slider.value;
      if(N==50){
-
+        bubbleSort(arr);
 
      }else{
         var myNode = document.querySelector(".sortgraph")
@@ -76,13 +144,14 @@ document.querySelector(".Startbtn").addEventListener("click",function(){
           myNode.removeChild(myNode.lastChild);
         }
         arr =[];
+        
         for (var i = 1; i <= N; i++) {
             arr.push(i);
         }
         arr= shuffle(arr);
         console.log(arr);
         cratebargraph(arr,N);
-
+       bubbleSort(arr);
      }
      
     
